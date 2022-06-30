edeaf6710d06e6be70bbb745731e315f733fe1f0
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-06-30 17:55:05 +0200

    feat: implement correct sigma for mice.impute.pls

--- R/mice.impute.pls.R
+++ R/mice.impute.pls.R
@@ -12,16 +12,14 @@
 #' \enumerate{
 #' \item A bootstrap sample of the observed part of the data is taken
 #' \item PLSR is estimated on this sample for a given number of latent variables (nlvs).
-#' \item Based on this PLSR model, predictions on the new data x[!ry, ]
+#' \item Based on this PLSR model, predictions on the new data x[!ry, ] are returned
 #' \item Imputations are then obtained by adding noise to these predictions.
 #' }
 #' 
 #' Noise is added by sampling from a standard normal distribution rescaled based 
 #' on the residuals standard error of the PLSR.
-#' The residual degrees of freedom (df) are computed according to a naive approach 
-#' (n - nlvs - 1).
-#' Future developments of this function could include one of the more elaborate 
-#' approaches to estimating df described in Kramer and Sugiyama (2011).
+#' The residual degrees of freedom (df) are computed based on Krylov representation 
+#' described by Kramer and Sugiyama (2011).
 #' 
 #' @author Edoardo Costantini, 2022
 #' @references
@@ -38,7 +36,7 @@
 mice.impute.pls <- function(y, ry, x, wy = NULL, nlvs = 1L, ...) {
 
     # Set up    
-    install.on.demand("pls", ...)
+    install.on.demand("plsdof", ...)
     if (is.null(wy)) wy <- !ry
 
     # Take bootstrap sample for model uncertainty
@@ -47,26 +45,21 @@ mice.impute.pls <- function(y, ry, x, wy = NULL, nlvs = 1L, ...) {
     dotxobs <- x[ry, , drop = FALSE][s, ]
     dotyobs <- y[ry][s]
 
-    # Train PLS on observed (bootsrap) data
-    pls_out <- pls::plsr(
-        y ~ .,
-        data = data.frame(y = dotyobs, dotxobs),
-        ncomp = nlvs,
-        validation = "none",
-    )
-    
-    # Predict new data
-    pls_pred <- as.matrix(
-        predict(pls_out, newdata = x[!ry, , drop = FALSE], ncomp = nlvs)
+    # Train PLS and get predictions
+    pls_fit <- plsdof::pls.model(
+        X = dotxobs,
+        y = dotyobs,
+        m = ncol(dotxobs),
+        compute.DoF = TRUE,
+        Xtest = x[!ry, , drop = FALSE], 
+        ytest = NULL
     )
 
-    # Compute residual standard error (sd of residuals with df as denominator) for PLS
-    res_ss <- sum(stats::resid(pls_out)^2)
-    res_df <- nrow(dotxobs) - nlvs - 1
-    sigma <- sqrt(res_ss / res_df)
+    # Extract residual standard error (sd of residuals with df as denominator) for PLS
+    sigma <- pls_fit$sigmahat[nlvs + 1]
 
     # Add noise for imputation uncertainty
-    imputes <- pls_pred + rnorm(sum(wy)) * sigma
+    imputes <- pls_fit$prediction[, nlvs + 1] + rnorm(sum(wy)) * sigma
 
     # Return
     return(imputes)
