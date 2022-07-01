f3adccbec195f0bd4f9b6fac5fba2d4727ee37ab
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-07-01 18:08:46 +0200

    feat: add Krylov representation to estimate DoF

--- R/mice.impute.pcovr.R
+++ R/mice.impute.pcovr.R
@@ -17,11 +17,18 @@
 #' \item Predicts y values based on \code{x[wy,]} and adds noise.
 #' }
 #' 
+#' Residual degrees of freedom are estimated by using the Krylov representation
+#' described by Kramer and Sugiyama (2011) as implemented in the \code{plsdof} 
+#' package.
+#' 
 #' @author Edoardo Costantini, 2022
 #' @references
 #'
 #' De Jong, S., & Kiers, H. A. (1992). Principal covariates regression: part I. 
 #' Theory. Chemometrics and Intelligent Laboratory Systems, 14(1-3), 155-164.
+#' 
+#' Krämer, N., & Sugiyama, M. (2011). The degrees of freedom of partial least
+#' squares regression. Journal of the American Statistical Association, 106(494), 697-705.
 #' 
 #' Vervloet, M., Kiers, H. A., Van den Noortgate, W., & Ceulemans, E. (2015). 
 #' PCovR: An R package for principal covariates regression. Journal of Statistical Software, 65, 1-14.
@@ -68,16 +75,31 @@ mice.impute.pcovr <- function(y, ry, x, wy = NULL, npcs = 1L, ...) {
     Hx <- dotxobs %*% solve(t(dotxobs) %*% dotxobs) %*% t(dotxobs)
     G <- alpha * dotxobs %*% t(dotxobs) / sum(dotxobs^2) + (1 - alpha) * Hx %*% dotyobs %*% t(dotyobs) %*% Hx / sum(dotyobs^2)
     EG <- eigen(G) # eigen-decomposition of matrix
-    Ts <- EG$vectors[, 1:npcs]
+    Ts <- EG$vectors [, 1:ncol(dotxobs), drop = FALSE]
     W <- solve(t(dotxobs) %*% dotxobs) %*% t(dotxobs) %*% Ts
     Py <- t(W) %*% t(dotxobs) %*% dotyobs
+    
+    # Extract fitted values for all numbers of pcs up untill npcs
+    Yhat <- sapply(1:ncol(dotxobs), function(j){
+        mean(dotyobs) + dotxobs %*% W[, 1:j, drop = FALSE] %*% Py[1:j, , drop = FALSE]
+    })
 
     # Predict new observations
-    y_hat <- mean(y[ry]) + xmis %*% W %*% Py
+    y_hat <- mean(y[ry]) + xmis %*% W[, 1:npcs, drop = FALSE] %*% Py[1:npcs, , drop = FALSE]
 
-    # Compute residual standard error (sd of residuals with df as denominator) for PLS
+    # Compute residual standard error (sd of residuals with df as the denominator) for PLS
+    DoFs <- dofPLS(
+        X = dotxobs,
+        y = dotyobs,
+        TT = Ts,
+        Yhat = Yhat,
+        m = ncol(dotxobs),
+        DoF.max = ncol(dotxobs) + 1
+    )
+
+    # Compute sigma    
     res_ss <- sum((y[ry] - mean(y[ry]) + dotxobs %*% W %*% Py)^2)
-    res_df <- nrow(dotxobs) - npcs - 1
+    res_df <- nrow(dotxobs) - DoFs[npcs]
     sigma <- sqrt(res_ss / res_df)
 
     # Add noise to make imputations
