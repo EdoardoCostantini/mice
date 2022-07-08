f7cc495234411052fa222bdc4c05a9f142aa4c3f
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-07-08 14:27:31 +0200

    feat: use more stable plsr

--- R/mice.impute.pls.R
+++ R/mice.impute.pls.R
@@ -5,6 +5,7 @@
 #' @aliases mice.impute.pls pls
 #' @inheritParams mice.impute.norm.boot
 #' @param nlvs The number of latent variables to consider in the PLS regression.
+#' @param DoF The method to compute the degrees of freedom, etiher \code{"naive"}, or \code{"kramer"}.
 #' @return Vector with imputed data, same type as \code{y}, and of length
 #' \code{sum(wy)}
 #' @details
@@ -23,7 +24,7 @@
 #' on the residuals standard error of the PLSR model.
 #' The residual degrees of freedom are computed based on the Krylov representation 
 #' described by Kramer and Sugiyama (2011) as implemented in the R package \code{plsdof} 
-#' (Krämer and Sugiyama, 2011).
+#' (Krämer and Sugiyama, 2011) or in the naive approach (n - nlvs - 1).
 #' 
 #' @author Edoardo Costantini, 2022
 #' @references
@@ -37,10 +38,10 @@
 #' @family univariate imputation functions
 #' @keywords datagen
 #' @export
-mice.impute.pls <- function(y, ry, x, wy = NULL, nlvs = 1L, ...) {
+mice.impute.pls <- function(y, ry, x, wy = NULL, nlvs = 1L, DoF = "naive", ...) {
 
     # Set up    
-    install.on.demand("plsdof", ...)
+    install.on.demand("plsdof", "pls", ...)
     if (is.null(wy)) wy <- !ry
 
     # Take bootstrap sample for model uncertainty
@@ -49,21 +50,48 @@ mice.impute.pls <- function(y, ry, x, wy = NULL, nlvs = 1L, ...) {
     dotxobs <- x[ry, , drop = FALSE][s, ]
     dotyobs <- y[ry][s]
 
-    # Train PLS and get predictions
-    pls_fit <- plsdof::pls.model(
-        X = dotxobs,
-        y = dotyobs,
-        m = ncol(dotxobs),
-        compute.DoF = TRUE,
-        Xtest = x[!ry, , drop = FALSE], 
-        ytest = NULL
+    # Train PLS on observed (bootsrap) data
+    pls_out <- pls::plsr(
+        y ~ .,
+        data = data.frame(y = dotyobs, dotxobs),
+        ncomp = ncol(dotxobs),
+        validation = "none",
     )
 
-    # Extract residual standard error (sd of residuals with df as denominator) for PLS
-    sigma <- pls_fit$sigmahat[nlvs + 1]
+    # Predict new data
+    pls_pred <- as.matrix(
+        predict(pls_out, newdata = x[!ry, , drop = FALSE], ncomp = nlvs)
+    )
+
+    # Compute residual sum of squares
+    res_ss <- sum((resid(pls_out)[, , nlvs])^2)
+
+    # Compute degrees of freedom
+    if(DoF == "naive"){
+        res_df <- nrow(dotxobs) - nlvs - 1
+    }
+    if(DoF == "kramer"){
+        # Extract fitted values for all numbers of pcs
+        Yhat <- sapply(1:ncol(dotxobs), function(j) {
+            predict(pls_out, ncomp = j)
+        })
+        # Compute DoFs
+        DoF_plsr <- .dofPLS(
+            X = dotxobs,
+            y = dotyobs,
+            TT = apply(pls::scores(pls_out), 2, function(j) j / sqrt(sum(j^2))),
+            Yhat = Yhat,
+            m = ncol(dotxobs),
+            DoF.max = ncol(dotxobs) + 1
+        )
+        res_df <- nrow(dotxobs) - DoF_plsr[nlvs + 1]
+    }
+
+    # Compute sigma
+    sigma <- sqrt(res_ss / res_df)
 
     # Add noise for imputation uncertainty
-    imputes <- pls_fit$prediction[, nlvs + 1] + rnorm(sum(wy)) * sigma
+    imputes <- pls_pred + rnorm(sum(wy)) * sigma
 
     # Return
     return(imputes)
