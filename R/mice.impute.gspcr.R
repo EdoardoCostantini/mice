70d281f407a56ae5b629a21753ae50333a3993a2
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-02-16 14:10:36 +0100

    feat: make cross validation as function in gspcr

--- R/mice.impute.gspcr.R
+++ R/mice.impute.gspcr.R
@@ -87,114 +87,23 @@ mice.impute.gspcr <- function(y, ry, x, wy = NULL,
 
   # Cross-validate threshold ---------------------------------------------------
 
-  # Family model
-  fam <- "gaussian"
-
-  # Fit null model
-  glm0 <- glm(dotyobs ~ 1, family = fam)
-
-  # Fit univariate models
-  glm.fits <- lapply(1:ncol(dotxobs), function(j) {
-      glm(dotyobs ~ dotxobs[, j], family = fam)
-  })
-
-  # Extract Log-likelihood values
-  ll0 <- as.numeric(logLik(glm0))
-  lls <- sapply(glm.fits, function(m) as.numeric(logLik(m)))
-
-  # Define log-likelihood threshold values
-  ll_thrs <- seq(from = min(lls), to = max(lls), length.out = nthrs)
-
-  # Create a map of active predictors for threshold values
-  map_active <- sapply(1:nthrs, function(a) lls > ll_thrs[a])
-
-  # Create an object to store k-fold cross-validation log-likelihoods
-  map_kfll <- array(dim = c(maxnpcs, nthrs, K))
-
-  # Create a fold partitioning object
-  part <- sample(rep(1:K, ceiling(nrow(dotxobs) / K)))[1:nrow(dotxobs)]
-
-  # Compute cross-validation measures
-  # Loop over K folds
-  for (k in 1:K) {
-    # Create fold data:
-    Xtr <- dotxobs[part != k, , drop = FALSE]
-    Xva <- dotxobs[part == k, , drop = FALSE]
-    ytr <- dotyobs[part != k]
-    yva <- dotyobs[part == k]
-
-    # Loop over threshold values
-    for (thr in 1:nthrs) {
-      # thr <- 1
-      # Define the active set of predictors based on the current threshold value
-      aset <- map_active[, thr]
-
-      # If there is more than 1 active variable
-      if (sum(aset) > 1) {
-        # Scale Xs
-        Xtr_thr <- scale(Xtr[, aset], center = TRUE, scale = TRUE)
-        Xva_thr <- scale(Xva[, aset],
-          center = attributes(Xtr_thr)$`scaled:center`,
-          scale = attributes(Xtr_thr)$`scaled:scale`
-        )
-
-        # Perform PCA
-        svd_Xtr <- svd(Xtr_thr)
-
-        # Project validation data on the PCs
-        PCsva <- Xva_thr %*% svd_Xtr$v # TODO: check project is done simply with V
-
-        # Check how many components are available (effective number)
-        q.eff <- min(sum(aset), maxnpcs)
-
-        # if (sum(aset) >= maxnpcs) {
-          # q.eff <- maxnpcs
-
-          # Select the PC scores that are available
-          PCsva.eff <- PCsva[, 1:q.eff, drop = FALSE]
-
-          # Compute the F-statistic for the possible additive PCRs
-          for (Q in 1:q.eff) {
-            # For the k-th PCs
-            # Q <- 1
-
-            # Estimate GLM
-            glm.fit <- glm(yva ~ PCsva.eff[, 1:Q], family = fam)
-
-            # Store the F statistic (used as a scaled value of the chi-square stat)
-            map_kfll[Q, thr, k] <- as.numeric(logLik(glm.fit))
-          }
-        # }
-      }
-    }
-  }
-
-  # K-fold cross-validation log-likelihood scores
-  map_kfll_avg <- apply(map_kfll, c(1, 2), mean, na.rm = FALSE)
-
-  # Put on likelihood scale
-  map_kfll_avg_l <- exp(map_kfll_avg)
-
-  # K-fold Cross-Validation Choice
-  coord_KCVC <- which(
-    map_kfll_avg_l == max(map_kfll_avg_l, na.rm = TRUE), #TODO: min or max?
-    arr.ind = TRUE
+  # Cross-validate the npcs and threshold
+  spcr.cv <- .spcr.cv(
+    dv = dotyobs,
+    ivs = dotxobs,
+    fam = "gaussian",
+    nthrs = 10
   )
 
-  # Which threshold has been selected?
-  thr.cv <- coord_KCVC[, "col"]
-
-  # How many npcs have been selected
-  Q.cv <- coord_KCVC[, "row"]
- 
   # Train PCR on dotxobs sample
   pcr_out <- pls::pcr(
-    dotyobs ~ dotxobs[, map_active[, thr.cv], drop = FALSE],
-    ncomp = Q.cv,
+    dotyobs ~ dotxobs[, spcr.cv$pred.active, drop = FALSE],
+    ncomp = spcr.cv$Q.cv,
     scale = TRUE,
     center = TRUE,
     validation = "none"
   )
+  # TODO: make this your function
 
   # Compute the residual sum of squares
   res_ss <- sum(stats::resid(pcr_out)[, , Q.cv]^2)
@@ -218,4 +127,132 @@ mice.impute.gspcr <- function(y, ry, x, wy = NULL,
 
   # Return
   return(imputes)
+}
+
+# Cross-validate function
+
+.spcr.cv <- function(dv, ivs, fam = "gaussian", nthrs = 10, maxnpcs = 3) {
+
+  # Example inputs
+  # dv <- as.factor(mtcars[, 9])
+  # ivs <- mtcars[, -9]
+  # nthrs = 10
+  # fam <- c("gaussian", "binomial", "poisson")[2]
+  # maxnpcs <- 3
+
+  # Fit null model
+  glm0 <- glm(dv ~ 1, family = fam)
+
+  # Fit univariate models
+  glm.fits <- lapply(1:ncol(ivs), function(j) {
+    glm(dv ~ ivs[, j], family = fam)
+  })
+  # TODO: check what happens if ivs have a categorical predictor
+  # TODO: check what happens if dv has different distributino
+  # TODO: if family gaussian, then do simple lm which is faster than glm
+
+  # Extract Log-likelihood values
+  ll0 <- as.numeric(logLik(glm0))
+  lls <- sapply(glm.fits, function(m) as.numeric(logLik(m)))
+
+  # Define log-likelihood threshold values
+  ll_thrs <- seq(from = min(lls), to = max(lls), length.out = nthrs)
+
+  # Create a map of active predictors for threshold values
+  map_active <- sapply(1:nthrs, function(a) lls > ll_thrs[a])
+
+  # Use threshold as name
+  colnames(map_active) <- ll_thrs
+
+  # If two thresholds are giving the same result reduce the burden
+  map_active <- map_active[, !duplicated(t(map_active))]
+  nthrs.eff <- ncol(map_active)
+  
+  # Create an object to store k-fold cross-validation log-likelihoods
+  map_kfll <- array(
+    dim = c(maxnpcs, nthrs.eff, K),
+    dimnames = list(NULL, colnames(map_active), NULL)
+  )
+
+  # Create a fold partitioning object
+  part <- sample(rep(1:K, ceiling(nrow(ivs) / K)))[1:nrow(ivs)]
+
+  # Compute cross-validation measures
+  # Loop over K folds
+  for (k in 1:K) {
+    # Create fold data:
+    Xtr <- ivs[part != k, , drop = FALSE]
+    Xva <- ivs[part == k, , drop = FALSE]
+    ytr <- dv[part != k]
+    yva <- dv[part == k]
+
+    # Loop over threshold values
+    for (thr in 1:nthrs.eff) {
+      # thr <- 1
+      # Define the active set of predictors based on the current threshold value
+      aset <- map_active[, thr]
+
+      # If there is more than 1 active variable
+      if (sum(aset) > 1) {
+        # Scale Xs
+        Xtr_thr <- scale(Xtr[, aset], center = TRUE, scale = TRUE)
+        Xva_thr <- scale(Xva[, aset],
+          center = attributes(Xtr_thr)$`scaled:center`,
+          scale = attributes(Xtr_thr)$`scaled:scale`
+        )
+
+        # Perform PCA
+        svd_Xtr <- svd(Xtr_thr)
+
+        # Project validation data on the PCs
+        PCsva <- Xva_thr %*% svd_Xtr$v # TODO: check project is done simply with V
+
+        # Check how many components are available (effective number)
+        q.eff <- min(sum(aset), maxnpcs)
+
+        # Select the PC scores that are available
+        PCsva.eff <- PCsva[, 1:q.eff, drop = FALSE]
+
+        # Compute the F-statistic for the possible additive PCRs
+        for (Q in 1:q.eff) {
+          # For the k-th PCs
+          # Q <- 1
+
+          # Estimate GLM
+          glm.fit <- glm(yva ~ PCsva.eff[, 1:Q], family = fam)
+
+          # Store the F statistic (used as a scaled value of the chi-square stat)
+          map_kfll[Q, thr, k] <- as.numeric(logLik(glm.fit))
+        }
+        # }
+      }
+    }
+  }
+
+  # K-fold cross-validation log-likelihood scores
+  map_kfll_avg <- apply(map_kfll, c(1, 2), mean, na.rm = FALSE)
+
+  # Put on likelihood scale
+  map_kfll_avg_l <- exp(map_kfll_avg)
+
+  # K-fold Cross-Validation Choice
+  coord_KCVC <- which(
+    map_kfll_avg_l == max(map_kfll_avg_l, na.rm = TRUE), # TODO: min or max?
+    arr.ind = TRUE
+  )
+
+  # Which threshold has been selected?
+  thr.cv <- as.numeric(colnames(map_kfll_avg_l)[coord_KCVC[, "col"]])
+
+  # How many npcs have been selected?
+  Q.cv <- coord_KCVC[, "row"]
+
+  # Return
+  return(
+    list(
+      thr.cv = thr.cv,
+      pred.active = map_active[, coord_KCVC[, "col"]],
+      Q.cv = Q.cv
+    )
+  )
 }
