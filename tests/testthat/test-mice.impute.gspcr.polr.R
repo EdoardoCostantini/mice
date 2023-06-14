c867bc1d7c1acb610923930cd5dbf0b984d38432
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-06-14 16:01:06 +0200

    feat: mice.impute.gspcr.polr

---
+++ tests/testthat/test-mice.impute.gspcr.polr.R
@@ -0,0 +1,34 @@
+# Test 1: Simple use -----------------------------------------------------------
+
+# Define the test context
+context("mice.impute.gspcr.polr: simple use")
+
+# Load example data
+data(GSPCRexdata, package = "gspcr")
+
+# Set a seed
+set.seed(20230614)
+
+# define an example predictor set
+x <- GSPCRexdata$X$cont
+
+# define an example dependent variable
+y <- GSPCRexdata$y$ord
+
+# make missingness
+y[sample(1:length(GSPCRexdata$y$ord), length(GSPCRexdata$y$ord) * .1)] <- NA
+
+# Define response indicator
+ry <- !is.na(y)
+
+# Define missingness indicator
+wy <- !ry
+
+# Use univariate imputation model
+imps_t1 <- mice.impute.gspcr.polr(y, ry, x, nthrs = 3)
+
+# Impute with standard polr
+imp_polr_standard <- mice.impute.polr(y, ry, x)
+
+# Test: returns same class as standard polr
+testthat::expect_equal(class(imp_polr_standard), class(imps_t1))
