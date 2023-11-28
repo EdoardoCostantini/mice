00124e74d700e1be8337983cce626a8cbd7e682a
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-11-28 15:45:08 +0100

    feat: add warning for pkg version upon loading

--- R/zzz.R
+++ R/zzz.R
@@ -14,3 +14,17 @@ version <- function(pkg = "mice") {
   d <- packageDescription(pkg)
   return(paste(d$Package, d$Version, d$Date, lib))
 }
+
+.onLoad <- function(libname, pkgname) {
+  warning(
+    paste0(
+      "\n",
+      version(), " was loaded.\n\n",
+      "This is an experimental version of the mice package developed by Edoardo Costantini as an unofficial fork to the main package. I created this version to demonstrate the potential use of generalised supervised principal components as a univariate imputation method in mice. I created this version in autonomy from maintainers and other contributors of the CRAN version of the `mice` R package. \n\n", 
+      "IMPORTANT!",
+      "\n\n",
+      "If you encounter any issues using it please contact me e.costantini@tilburguniversity.edu."
+    ),
+    call. = FALSE
+  )
+}
